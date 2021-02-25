b3b003ca77d5f845491875c00ef47500c2ea6377
lliang4188 <42561515@qq.com>
2021-02-25 18:44:39 +0800

    red-data

--- src/assets/js/libs/request.js
+++ src/assets/js/libs/request.js
@@ -1,10 +1,29 @@
 import ReactDom from 'react-dom';
-
-
-
 let onLoad = ReactDom.findDOMNode(document.getElementById('page-load'));
-function request (pUrl, pType= 'GET') {
+function request (pUrl, pType= 'GET'.toLocaleLowerCase(), data={}) {
   showLoad();
+  let config={}, headers={},params='';
+  if (pType === 'get'.toLocaleLowerCase()) {
+    config = {
+      method: pType
+    }
+  } else {
+    headers = {
+      'Content-Type':'application/x-www-form-urlencoded'
+    }
+    if (data instanceof Object) {
+      for (let key in data){
+        params+= `&${key}=${encodeURIComponent(data[key])}`;
+      }
+      params = params.slice(1);
+    }
+    config = {
+      method: pType,
+      headers,
+      body:params
+    }
+  }
+
   return fetch(pUrl, {method: pType}).then(res => {
     hideLoad();
     return res.json();
--- src/pages/home/reg/index.js
+++ src/pages/home/reg/index.js
@@ -1,11 +1,76 @@
 import React from 'react';
+import config from "../../../assets/js/conf/config";
+import {request} from "../../../assets/js/libs/request";
 import SubHeader from "../../../components/header/subheader";
+import { Toast } from 'antd-mobile';
 import Css from '../../../assets/css/home/reg/index.css';
 export default class RegIndex extends React.Component {
   constructor() {
     super();
     this.state= {
-      isChecked: false
+      isChecked: false,
+      sCellphone: '',
+      bCodeSuccess: false,
+      sCodeText: '获取短信验证码',
+      sCode: '',
+      sPassword:''
+    }
+    this.timer = null;
+    this.bSendCode = true;
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    this.setState = (state,callback) => {
+      return;
+    }
+  }
+  // 检查手机号
+  checkCellphone(e){
+    this.setState({sCellphone:e.target.value}, ()=>{
+      if(this.bSendCode){
+        if (this.state.sCellphone.match(/^1[3-9][0-9]{9}/)) {
+          this.setState({bCodeSuccess: true});
+        } else {
+          this.setState({bCodeSuccess: false});
+        }
+      }
+
+    });
+  }
+  // 获取验证码
+  getCode(){
+    if(this.bSendCode && this.state.bCodeSuccess) {
+      this.bSendCode = false;
+      let iTime = 10;
+      this.timer= setInterval(() => {
+        if(iTime>0){
+          iTime--;
+          this.setState({sCodeText:'重新发送('+ iTime+'s)', bCodeSuccess:false});
+        }else {
+          clearInterval(this.timer);
+          this.bSendCode = true;
+          this.setState({sCodeText:'获取短信验证码', bCodeSuccess:true});
+        }
+      }, 1000)
+    }
+  }
+  // 点击注册按钮数据
+  submitData(){
+    if(this.state.sCellphone.match(/^\s*$/)) {
+      Toast.info('请输入手机号',2);
+      return false;
+    }
+    if(!this.state.sCellphone.match(/^1[3-9][0-9]{9}/)){
+      Toast.info('请输入正确的手机号',2);
+      return false;
+    }
+    if(this.state.sCode.match(/^\s*$/)) {
+      Toast.info('请输入短信验证码',2);
+      return false;
+    }
+    if(this.state.sPassword.match(/^\s*$/)) {
+      Toast.info('请输入密码',2);
+      return false;
     }
   }
   render() {
@@ -15,24 +80,25 @@ export default class RegIndex extends React.Component {
           <div className={Css['main']}>
             <div className={Css['main-list']}>
               <div className={Css['input-wrap']}>
-                <input type="tel" placeholder="请输入手机号"/>
+                <input type="tel" placeholder="请输入手机号" onChange={(e) => {this.checkCellphone(e)}} disabled={this.bSendCode ? false : true}/>
               </div>
-              <div className={Css['btn-code']}>获取短信验证码</div>
+              <div className={this.state.bCodeSuccess ? Css['btn-code'] + ' ' + Css['success'] : Css['btn-code']} onClick={this.getCode.bind(this)}>{this.state.sCodeText}</div>
             </div>
             <div className={Css['main-list']}>
               <div className={Css['input-wrap']}>
-                <input type="text" placeholder="请输入短信验证码"/>
+                <input type="text" placeholder="请输入短信验证码" onChange={(e)=>{this.setState({sCode:e.target.value})}}/>
               </div>
             </div>
             <div className={Css['password-item']}>
               <div className={Css['password-input']}>
-                <input type={this.state.isChecked ? 'text': 'password'} placeholder="请输入密码"/>
+                <form>
+                <input type={this.state.isChecked ? 'text': 'password'} placeholder="请输入密码" onChange={(e)=>{this.setState({sPassword:e.target.value})}} autoComplete="off"/>
+                </form>
               </div>
               <div className={this.state.isChecked ? Css['eyes']+ ' ' + Css['open']: Css['eyes']} onClick={()=>{this.setState({isChecked:!this.state.isChecked})}}></div>
             </div>
-            <div className={Css['btn-sure']}>注册</div>
+            <div className={Css['btn-sure']} onClick={this.submitData.bind(this)}>注册</div>
           </div>
-
         </div>
     )
   }
